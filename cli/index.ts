e9e1d87801a342d53cc7e58ac4b285e1edb96094
lakewik <wikjezioro@op.pl>
2024-11-16 00:47:21 -0800

    Working CLI

--- cli/index.ts
+++ cli/index.ts
@@ -1,12 +1,15 @@
 // CLI for interfacing with SafeHODL //
 
-import { ethers, Wallet, Contract, ContractReceipt } from "ethers";
+import { ethers, Wallet, Contract, solidityPackedKeccak256 } from "ethers";
 import figlet from "figlet";
 import {Command} from "commander";
-import { env } from "bun";
+
 import chalk from 'chalk';
 import fs from 'fs';
 import path from 'path';
+
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 
 const readlineSync = require('readline-sync');
@@ -14,20 +17,24 @@ const readlineSync = require('readline-sync');
 const log = console.log;
 
 
-var callerPrivateKey = "";
+var callerPrivateKey = process.env.PRIVKEY;
 
 const program = new Command();
 
-const contract_address_polygon_sepolia = env.CONTRACT_ADDRESS_POLYGON_SEPOLIA;
-const contract_address_unichain_sepolia = env.CONTRACT_ADDRESS_UNICHAIN_SEPOLIA;
-const contract_address_mantle_sepolia = env.CONTRACT_ADDRESS_MANTLE_SEPOLIA;
+const contract_address_polygon_sepolia = process.env.CONTRACT_ADDRESS_POLYGON || "";
+const contract_address_unichain_sepolia = process.env.CONTRACT_ADDRESS_UNICHAIN || "";
+const contract_address_mantle_sepolia = process.env.CONTRACT_ADDRESS_MANTLE || "";
+
+const rpc_polygon = process.env.POLYGON_RPC_URL || "";
+const rpc_unichain = process.env.UNICHAIN_RPC_URL || "";
+const rpc_mantle = process.env.MANTLE_RPC_URL || "";
 
 
 const contractABIpath = path.join(__dirname, 'ABIs/abi.json');
 const contractABI = JSON.parse(fs.readFileSync(contractABIpath, 'utf8'));
 
 
-figlet("SafeHODL", function (err, data) {
+figlet("SafeHODL", async function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
@@ -37,40 +44,98 @@ figlet("SafeHODL", function (err, data) {
 
   const actionToDo = readlineSync.question(log(chalk.blue('Select action to do: (1 - Lock, 2 - Unlock)')));
 
-  if (actionToDo == 1) {
-      // Lock
-      const receiverEmail = readlineSync.question(log(chalk.blue('Provide receiver email address:')));
-      const password = readlineSync.question(log(chalk.blue('Provide unlocking password')));
+  const receiverEmail = "";
+  const password = "";
+  const amountToLock = "";
+  const receiverAddress = "";
 
+  const selectedChain = readlineSync.question(log(chalk.blue('Select chain: (1 - Polygon Sepolia, 2 - Mantle Sepolia, 3 - Unichain Sepolia)')));
 
-  } else if (actionToDo == 2) {
-      // Unlock
-      const receiverEmail = readlineSync.question(log(chalk.blue('Provide your email address:')));
-      const password = readlineSync.question(log(chalk.blue('Provide your password')));
-      const receiverAddress = readlineSync.question(log(chalk.blue('Provide destination address:')));
-      
+    if (selectedChain == 1) {
+        log(chalk.green('Selected Polygon Sepolia'))
+        // Polygon Sepolia
 
-  }
+        const provider = new ethers.JsonRpcProvider(rpc_polygon);
+        const wallet = new ethers.Wallet(callerPrivateKey as string, provider);
+        const safeHODLContract = new ethers.Contract(
+            contract_address_polygon_sepolia,
+            contractABI,
+            wallet
+        );
+        
+        await callContract(safeHODLContract, actionToDo);
 
-  const selectedChain = readlineSync.question(log(chalk.blue('Select chain: (1 - Polygon Sepolia, 2 - Unichain Sepolia, 3 - Mantle Sepolia)')));
+    } else if (selectedChain == 2) {
+        // Mantle Sepolia
 
-  if (selectedChain == 1) {
-    // Polygon Sepolia
+        const provider = new ethers.JsonRpcProvider(rpc_mantle);
+        const wallet = new ethers.Wallet(callerPrivateKey as string, provider);
+        const safeHODLContract = new ethers.Contract(
+            contract_address_mantle_sepolia,
+            contractABI,
+            wallet
+        );
 
-    const provider = new ethers.JsonRpcProvider(env.POLYGON_SEPOLIA_RPC_URL);
-    const wallet = new ethers.Wallet(callerPrivateKey as string, provider);
-    const safeHODLContract = new ethers.Contract(
-        contract_address_polygon_sepolia,
-        contractABI,
-        wallet
-    );
+        await callContract(safeHODLContract, actionToDo);
+
+    } else if (selectedChain == 3) {
+        // Unichain Sepolia
+
+        const provider = new ethers.JsonRpcProvider(rpc_unichain);
+        const wallet = new ethers.Wallet(callerPrivateKey as string, provider);
+        const safeHODLContract = new ethers.Contract(
+            contract_address_unichain_sepolia,
+            contractABI,
+            wallet
+        );
+
+        await callContract(safeHODLContract, actionToDo);
+
+    }
+
+    async function callContract(safeHODLContract, actionToDo) {
+        console.log(actionToDo);
+        if (actionToDo == 1) {
+            log(chalk.green('Selected Lock'))
+        
+            // Lock
+            const receiverEmail = readlineSync.question(log(chalk.blue('Provide receiver email address:')));
+            const password = readlineSync.question(log(chalk.blue('Provide unlocking password')));
+            const amountToLock = readlineSync.question(log(chalk.blue('Provide amount to lock')));
+    
+            // Offline calculate the locking hash
+            const lockingHash = solidityPackedKeccak256([ "string", "string" ], [ receiverEmail, password ]);
+    
+    
+            const tx = await safeHODLContract.lock(
+                receiverEmail,
+                lockingHash,
+                {
+                  value: String(amountToLock),
+                }
+            );
+    
+            log(chalk.blue('TX sent. Hash: ' + tx.hash));
+    
+        } else if (actionToDo == 2) {
+            // Unlock
+            const receiverEmail = readlineSync.question(log(chalk.blue('Provide your email address:')));
+            const password = readlineSync.question(log(chalk.blue('Provide your password')));
+            const receiverAddress = readlineSync.question(log(chalk.blue('Provide destination address:')));
+    
+            const tx = await safeHODLContract.unlock(
+                receiverEmail,
+                password,
+                receiverAddress
+            );
+    
+            log(chalk.blue('TX sent. Hash: ' + tx.hash));
+        }
+    }
 
    
 
-
-
-
-}
+   
 
 });
 
