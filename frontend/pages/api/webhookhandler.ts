46bb58e2e01574a1723b4d6b861af4f329f44ba0
beeinger <sulecki.maciek@gmail.com>
2024-11-16 18:25:46 +0700

    ts fixes

--- frontend/pages/api/webhookhandler.ts
+++ frontend/pages/api/webhookhandler.ts
@@ -1,61 +1,58 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
 
-
 const mailerSend = new MailerSend({
-    apiKey: process.env.MAILERSEND_API_KEY,
+  apiKey: process.env.MAILERSEND_API_KEY || "",
 });
 
-
 // - mailersendapikey
-function sendNewLockboxCreatedNotificationEmail(newLockboxEmail, lockedBy) {
+function sendNewLockboxCreatedNotificationEmail(
+  newLockboxEmail: string,
+  lockedBy: string
+) {
+  const sentFrom = new Sender(
+    "trial-z86org8vz6zlew13.mlsender.net",
+    "SafeHODL notification service"
+  );
 
-    const sentFrom = new Sender("trial-z86org8vz6zlew13.mlsender.net", "SafeHODL notification service");
+  const recipients = [new Recipient(newLockboxEmail, "Your Client")];
 
-    const recipients = [
-      new Recipient(newLockboxEmail, "Your Client")
-    ];
-    
-    
-    const emailParams = new EmailParams()
-      .setFrom(sentFrom)
-      .setTo(recipients)
-      .setSubject("You have incoming blockchain transfer")
-      .setHtml(`You can unlock vault and receive locked tokens. Locked by: ${lockedBy} `)
-      .setText(`This is the text content`);
-    
-    mailerSend.email
-        .send(emailParams)
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
+  const emailParams = new EmailParams()
+    .setFrom(sentFrom)
+    .setTo(recipients)
+    .setSubject("You have incoming blockchain transfer")
+    .setHtml(
+      `You can unlock vault and receive locked tokens. Locked by: ${lockedBy} `
+    )
+    .setText(`This is the text content`);
 
-      
-
+  mailerSend.email
+    .send(emailParams)
+    .then((response: unknown) => console.log(response))
+    .catch((error: unknown) => console.log(error));
 }
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     // Process a POST request
 
-    var webhookContent = JSON.parse(req.body);
+    const webhookContent = JSON.parse(req.body);
     console.log(webhookContent);
-    
+
     if (webhookContent[0].data.event.name == "NewLockBox") {
-        if ( webhookContent[0].data.event.inputs[0].name == "email") {
-            var newLockboxEmail = webhookContent[0].data.event.inputs[0].value;
-            console.log(newLockboxEmail);
-            //var lockedAmount = 
-            var lockedBy = webhookContent[0].data.transaction.from;
-            sendNewLockboxCreatedNotificationEmail(newLockboxEmail, lockedBy);
-        }
+      if (webhookContent[0].data.event.inputs[0].name == "email") {
+        const newLockboxEmail = webhookContent[0].data.event.inputs[0].value;
+        console.log(newLockboxEmail);
+        //var lockedAmount =
+        const lockedBy = webhookContent[0].data.transaction.from;
+        sendNewLockboxCreatedNotificationEmail(newLockboxEmail, lockedBy);
+      }
     }
 
-    res.json({success:true});
-    
-
+    res.json({ success: true });
   } else {
     // Handle any other HTTP method
-  
-   res.json({success:true});
+
+    res.json({ success: true });
   }
 }
