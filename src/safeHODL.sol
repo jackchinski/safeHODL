ae1c6f6288a56caa02583783da1487e5aebe47e7
jackchinski <marcin.jacz@gmail.com>
2024-11-16 13:42:44 +0700

    unlock function

--- src/safeHODL.sol
+++ src/safeHODL.sol
@@ -7,6 +7,7 @@ contract SafeHodl {
 
     // events
     event NewLockBox(string email);
+    event LockBoxOpened(string email); 
 
     struct LockBox {
         bytes32 identifier; // hash of email and password
@@ -25,4 +26,29 @@ contract SafeHodl {
         emit NewLockBox(_email);
     }
 
+        function unlock(string calldata _email, string calldata _password, address recipientAddress) public payable {
+        require(recipientAddress != address(0), "Address cannot be empty");
+        require(bytes(_email).length > 0, "Email cannot be empty");
+        require(bytes(_password).length > 0, "Email cannot be empty");
+
+        // hash the email and password
+        bytes32 passwordAndEmailHash = keccak256(abi.encodePacked(_email, _password));
+
+        LockBox memory correctLockBox = vault[passwordAndEmailHash]; 
+
+        // check that the hash exists in the storage
+        require(vault[passwordAndEmailHash] != 0, "There is no Lockbox with the following email and password.")
+
+        // check that the hashes match 
+        require(correctLockBox.identifier == passwordAndEmailHash, "Hashes do not match!");
+        // check that the contract has enough funds to make the transfer
+        require(address(this).balance >= correctLockBox.amount)
+
+        // make the transfer to the destination address
+        (bool success,) = payable(recipientAddress).call{value: correctLockBox.amount}("");
+        require(success, "Paying out the user failed"); 
+
+        // emit an event that everything went through 
+        emit LockBoxOpened(_email); 
+    }
 }
